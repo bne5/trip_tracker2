3c4cecd05ad300aa62f8e17a5589fcb2e3c68315
Abigail Mitchell <abby.w.mitchell@gmail.com>
2019-12-07 10:54:49 -0700

    finished location controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,13 +1,58 @@
 class LocationsController < ApplicationController
+  before_action :set_trip
+  before_action :set_location, only: [:show, :edit, :update, :destroy]
+
   def index
+    @locations = @trip.locations
   end
 
   def show
   end
 
   def new
+    @location = @trip.locations.new
+    render partial: "form"
   end
 
   def edit
+    render partial: "form"
   end
+
+  def create
+    @location = @trip.locations.new(location_params)
+    if @location.save
+      redirect_to trip_location_path(@trip, @location)
+    else
+      render :new
+    end
+  end
+
+  def update
+    #PUT	/subs/:sub_id/topics/:id
+    if @location.update(location_params)
+      redirect_to trip_location_path(@trip)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @location.destroy
+    redirect_to trip_locations_path(@trip)
+  end
+
+  private
+
+    def set_location
+      @location = Location.find(params[:id])
+    end
+
+    def set_trip
+      @trip = Trip.find(params[:trip_id])
+    end
+
+    def location_params
+      params.require(:location).permit(:name, :days)
+    end
+
 end
